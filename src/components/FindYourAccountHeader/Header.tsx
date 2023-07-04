8c25474b663a8e23b626113510d0dacf783ac845
Kashif Sheikh <kashifshaikh910ssg@gmail.com>
2023-07-04 22:10:42 +0500

    Some changes in FindYourAccount screen Header

--- src/components/FindYourAccountHeader/Header.tsx
+++ src/components/FindYourAccountHeader/Header.tsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import FacebookImg from "../images/images.png";
+import TextInput from "../TextInput/TextInput";
 
 const Header = () => {
   return (
@@ -14,7 +15,51 @@ const Header = () => {
       <div className="findYourAccount-section-two-container">
         <div className="findYourAccount-section-two-main">
           <form action="/">
-            <div className="findYourAccount-inside-form"></div>
+            <div className="findYourAccount-inside-form">
+              <div className="findYourAccount-emailOrPhone-input-container">
+                <label>
+                  <TextInput
+                    type="email"
+                    className="findYourAccount-emailOrPhone-input"
+                    placeholder="Email or phone"
+                    name="email"
+                  />
+                </label>
+              </div>
+              <div className="findYourAccount-emailOrPhone-input-container">
+                <label>
+                  <TextInput
+                    type="password"
+                    className="findYourAccount-emailOrPhone-input"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </label>
+              </div>
+              <div className="findYourAccount-emailOrPhone-input-container">
+                <div>
+                  <div className="findYourAccount-login-button-container">
+                    <div className="findYourAccount-login-button-content">
+                      <div className="findYourAccount-login-button-main">
+                        <span className="findYourAccount-login-button">
+                          Log in
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="findYourAccount-emailOrPhone-input-container">
+                <span className="findYourAccount-forgotten-account-container">
+                  <a
+                    href="/"
+                    className="findYourAccount-forgotten-account-text"
+                  >
+                    Forgotten account?
+                  </a>
+                </span>
+              </div>
+            </div>
           </form>
         </div>
       </div>
